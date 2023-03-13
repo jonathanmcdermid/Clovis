3300afb3c99c42f4e5ae8b738acbe7b798780960
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2023-03-13 16:02:09 -0600

    fix some parsing bugs

--- src/parse.cpp
+++ src/parse.cpp
@@ -6,24 +6,24 @@ namespace Clovis {
 
 		Move parse(const Position& pos, string move)
 		{
+			Move res = MOVE_NONE;
+
 			if (move[move.length() - 1] == '+' || move[move.length() - 1] == '#')
 				move = move.substr(0, move.length() - 1);
 
 			if (move == "O-O-O")
 			{
-				return (pos.side == WHITE)
+				res = (pos.side == WHITE)
 					? encode_move(E1, C1, W_KING, NO_PIECE, 0, 0, 0, 1) 
 					: encode_move(E8, C8, B_KING, NO_PIECE, 0, 0, 0, 1);
 			}
-
-			if (move == "O-O")
+			else if (move == "O-O")
 			{
-				return (pos.side == WHITE)
+				res = (pos.side == WHITE)
 					? encode_move(E1, G1, W_KING, NO_PIECE, 0, 0, 0, 1)
 					: encode_move(E8, G8, B_KING, NO_PIECE, 0, 0, 0, 1);
 			}
-
-			if (islower(move[0]))
+			else if (islower(move[0]))
 			{
 				Piece promotion = 
 					  move[move.length() - 2] == '='
@@ -36,7 +36,7 @@ namespace Clovis {
 				{
 					Square to = str2sq(move.substr(2, 2));
 					Square from = make_square(File(move[0] - 'a'), rank_of(to - pawn_push(pos.side)));
-					return encode_move(from, to, make_piece(PAWN, pos.side), promotion, 1, 0, pos.bs->enpassant == to, 0);
+					res = encode_move(from, to, make_piece(PAWN, pos.side), promotion, 1, 0, pos.bs->enpassant == to, 0);
 				}
 				else
 				{
@@ -44,57 +44,92 @@ namespace Clovis {
 					Square from = pos.pc_table[to - pawn_push(pos.side)] == NO_PIECE 
 						? to - 2 * pawn_push(pos.side) 
 						: to - pawn_push(pos.side);
-					return encode_move(from, to, make_piece(PAWN, pos.side), promotion, 0, abs(rank_of(to) - rank_of(from)) == 2, 0, 0);
+					res = encode_move(from, to, make_piece(PAWN, pos.side), promotion, 0, abs(rank_of(to) - rank_of(from)) == 2, 0, 0);
 				}
 			}
+			else
+			{
+				Piece piece = make_piece(
+					  move[0] == 'K' ? KING
+					: move[0] == 'Q' ? QUEEN
+					: move[0] == 'R' ? ROOK
+					: move[0] == 'B' ? BISHOP
+					: KNIGHT, pos.side);
 
-			Piece piece = make_piece( 
-				    move[0] == 'K' ? KING
-				  : move[0] == 'Q' ? QUEEN
-				  : move[0] == 'R' ? ROOK
-				  : move[0] == 'B' ? BISHOP
-				  : KNIGHT, pos.side);
+				if (move[1] == 'x')
+				{
+					Square to = str2sq(move.substr(2, 2));
+					Bitboard bb = Bitboards::get_attacks(piece_type(piece), pos.occ_bb[BOTH], to) & pos.pc_bb[piece];
+					Square from = pop_lsb(bb);
+					if (bb)
+					{
+						// one of the pieces that attacks this square is pinned
+						if (pos.side == WHITE)
+							while (pos.get_pinner<WHITE>(from) != SQ_NONE)
+								from = pop_lsb(bb);
+						else
+							while (pos.get_pinner<BLACK>(from) != SQ_NONE)
+								from = pop_lsb(bb);
+					}
+					res = encode_move(from, to, piece, NO_PIECE, 1, 0, 0, 0);
+				}
+				else if (move[2] == 'x')
+				{
+					Square to = str2sq(move.substr(3, 2));
+					Bitboard bb = Bitboards::get_attacks(piece_type(piece), pos.occ_bb[BOTH], to) & pos.pc_bb[piece];
+					Square from = pop_lsb(bb);
+					if (isdigit(move[1]))
+						while (rank_of(from) != Rank(move[1] - '1'))
+							from = pop_lsb(bb);
+					else
+						while (file_of(from) != File(move[1] - 'a'))
+							from = pop_lsb(bb);
+					res = encode_move(from, to, piece, NO_PIECE, 1, 0, 0, 0);
+				}
+				else if (move.length() == 3)
+				{
+					Square to = str2sq(move.substr(1, 2));
+					Bitboard bb = Bitboards::get_attacks(piece_type(piece), pos.occ_bb[BOTH], to) & pos.pc_bb[piece];
+					Square from = pop_lsb(bb);
+					if (bb)
+					{
+						// one of the pieces that attacks this square is pinned
+						if (pos.side == WHITE)
+							while (pos.get_pinner<WHITE>(from) != SQ_NONE)
+								from = pop_lsb(bb);
+						else
+							while (pos.get_pinner<BLACK>(from) != SQ_NONE)
+								from = pop_lsb(bb);
+					}
+					res = encode_move(from, to, piece, NO_PIECE, 0, 0, 0, 0);
+				}
+				else if (move.length() == 4)
+				{
+					Square to = str2sq(move.substr(2, 2));
+					Bitboard bb = Bitboards::get_attacks(piece_type(piece), pos.occ_bb[BOTH], to) & pos.pc_bb[piece];
+					Square from = pop_lsb(bb);
+					if (isdigit(move[1]))
+						while (rank_of(from) != Rank(move[1] - '1'))
+							from = pop_lsb(bb);
+					else
+						while (file_of(from) != File(move[1] - 'a'))
+							from = pop_lsb(bb);
+					res = encode_move(from, to, piece, NO_PIECE, 0, 0, 0, 0);
+				}
+				else if (move.length() == 5)
+				{
+					Square to = str2sq(move.substr(3, 2));
+					Square from = str2sq(move.substr(1, 2));
+					res = encode_move(from, to, piece, NO_PIECE, 0, 0, 0, 0);
+				}
+			}
+			
+			MoveGen::MoveList ml = MoveGen::MoveList(pos);
+			ml.remove_illegal(pos);
 
-			if (move[1] == 'x')
-			{
-				Square to = str2sq(move.substr(2, 2));
-				Square from = lsb(Bitboards::get_attacks(piece_type(piece), pos.occ_bb[BOTH], to) & pos.pc_bb[piece]);
-				return encode_move(from, to, piece, NO_PIECE, 1, 0, 0, 0);
-			}
-			else if (move[2] == 'x')
-			{
-				Square to = str2sq(move.substr(3, 2));
-				Bitboard bb = Bitboards::get_attacks(piece_type(piece), pos.occ_bb[BOTH], to) & pos.pc_bb[piece];
-				Square from = pop_lsb(bb);
-				if (isdigit(move[1]))
-					while (rank_of(from) != Rank(move[1] - '1'))
-						from = pop_lsb(bb);
-				else
-					while (file_of(from) != File(move[1] - 'a'))
-						from = pop_lsb(bb);
-				return encode_move(from, to, piece, NO_PIECE, 1, 0, 0, 0);
-			}
-			else if (move.length() == 3)
-			{
-				Square to = str2sq(move.substr(1, 2));
-				Square from = lsb(Bitboards::get_attacks(piece_type(piece), pos.occ_bb[BOTH], to) & pos.pc_bb[piece]);
-				return encode_move(from, to, piece, NO_PIECE, 0, 0, 0, 0);
-			}
-			else if (move.length() == 4)
-			{
-				Square to = str2sq(move.substr(2, 2));
-				Bitboard bb = Bitboards::get_attacks(piece_type(piece), pos.occ_bb[BOTH], to) & pos.pc_bb[piece];
-				Square from = pop_lsb(bb);
-				while (file_of(from) != File(move[1] - 'a'))
-					from = pop_lsb(bb);
-				return encode_move(from, to, piece, NO_PIECE, 0, 0, 0, 0);
-			}
-			else if(move.length() == 5)
-			{
-				Square to = str2sq(move.substr(3, 2));
-				Square from = str2sq(move.substr(1, 2));
-				return encode_move(from, to, piece, NO_PIECE, 0, 0, 0, 0);
-			}
+			for (const auto& m : ml)
+				if (m == res)
+					return res;
 
 			exit(EXIT_FAILURE);
 		}
@@ -137,24 +172,28 @@ namespace Clovis {
 				}
 
 				Position pos(fen.c_str());
+				int fmc = 1;
+				Colour turn = WHITE;
+				bool live = true;
 
-				while (getline(ifs, line))
+				while (live && getline(ifs, line))
 				{
 					istringstream ss(line);
-					int fmc = 1;
-					Colour turn = WHITE;
-					while (line.find(to_string(fmc) + ". ") != string::npos)
+					while (line.find(to_string(fmc) + ". ") != string::npos || turn == BLACK || line.find(result) != string::npos)
 					{
 						string token;
 						ss >> skipws >> token;
+						if (token.empty())
+							break;
 						if (token == result)
 						{
-							cout << result << token << endl;
+							live = false;
 							break;
 						}
 						if (token.find(".") == string::npos)
 						{
-							pos.do_move(parse(pos, token));
+							if (!pos.do_move(parse(pos, token)))
+								exit(EXIT_FAILURE);
 							if (!(Random::random_U64() % 5))
 							{
 								Search::SearchLimits limits;
