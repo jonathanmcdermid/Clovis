c273649816191d010b2393af31322e74997574ee
Jonathan McDermid <admin@jon.engineering>
2023-04-06 11:39:55 -0600

    32 squares for candidate passer

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -6,6 +6,7 @@ namespace Clovis {
 
 		const Score* piece_table[7] = { NULL, pawn_table, knight_table, bishop_table, rook_table, queen_table, king_table };
 		const Score* passed_table[COLOUR_N][SQ_N];
+		const Score* candidate_table[COLOUR_N][SQ_N];
 		const short* shield_table[COLOUR_N][SQ_N];
 		const Score* score_table[15][SQ_N];
 
@@ -27,6 +28,7 @@ namespace Clovis {
 					//	score_table[make_piece(pt, col)][sq] = &piece_table[pt][source10[sq]];
 				
 					passed_table[col][sq] = &passed_pawn[source32[relative_square(col, sq)]];
+					candidate_table[col][sq] = &candidate_passer[source32[relative_square(col, sq)]];
 					shield_table[col][sq] = &pawn_shield[source32[relative_square(col, sq)]];
 				}
 			}
@@ -143,8 +145,7 @@ namespace Clovis {
 				if (pinner != SQ_NONE)
 					attacks &= between_squares(ei.ksq[US], pinner) | pinner;
 
-				Bitboard trades = worthy_trades<US, PT>(pos);
-				Bitboard safe_attacks = attacks & (~ei.pawn_attacks[~US] | trades);
+				Bitboard safe_attacks = attacks & (~ei.pawn_attacks[~US] | worthy_trades<US, PT>(pos));
 
 				score += quiet_mobility[PT]   * popcnt(safe_attacks & ~pos.occ_bb[BOTH]);
 				score += capture_mobility[PT] * popcnt(safe_attacks &  pos.occ_bb[~US]);
@@ -318,8 +319,8 @@ namespace Clovis {
 				}
 				else if (is_candidate_passer<US>(pos.pc_bb[OUR_PAWN], pos.pc_bb[THEIR_PAWN], sq))
 				{
-					score += candidate_passer[relative_rank(US, rank_of(sq))];
-					if constexpr (TRACE) ++T[CANDIDATE_PASSER + relative_rank(US, rank_of(sq))][US];
+					score += *candidate_table[US][sq];
+					if constexpr (TRACE) ++T[CANDIDATE_PASSER + source32[relative_square(US, sq)]][US];
 				}
 
 				king_danger<US, PAWN, TRACE>(sqbb(sq + pawn_push(US)), ei);
