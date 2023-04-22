88a2efa155f30aab37b1dbaece3e08af81cfef2a
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2023-04-21 21:03:22 -0600

    constexpr slider attacks

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ using namespace Clovis;
 int main(int argc, char* argv[]) {
 
 	Random::seed_random(0ULL);
-	Bitboards::init_bitboards();
 	Search::init_search();
 	Eval::init_eval();
 	Zobrist::init_zobrist();
