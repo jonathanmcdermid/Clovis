c3e3b2ed81f35c7558211356701260cdc7c51d40
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2023-05-08 19:15:47 -0600

    modernize with string view casting

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,18 @@ int main(const int argc, char* argv[]) {
 
 	clovis::bitboards::init_bitboards();
 
-	if (argc < 2)                       clovis::uci::loop(argc, argv);
-	else if (!strcmp(argv[1], "bench")) clovis::bench::benchmark(argc, argv);
-	else if (!strcmp(argv[1], "perft")) clovis::perft::perft();
-	else if (!strcmp(argv[1], "iq"))    clovis::iq::iq_test();
-	else if (!strcmp(argv[1], "tune"))  clovis::tuner::tune_eval();
-	else if (!strcmp(argv[1], "gen"))   clovis::parse::generate_data();
-	else if (!strcmp(argv[1], "--help"))
+	const std::string_view arg = argc > 1 ? argv[1] : "";
+
+	if (argc < 2)            clovis::uci::loop(argc, argv);
+	else if (arg == "bench") clovis::bench::benchmark(argc, argv);
+	else if (arg == "perft") clovis::perft::perft();
+	else if (arg == "iq")    clovis::iq::iq_test();
+	else if (arg == "tune")  clovis::tuner::tune_eval();
+	else if (arg == "gen")   clovis::parse::generate_data();
+	else if (arg == "--help")
 		std::cout << "bench [depth=" << clovis::DEFAULT_BENCH_DEPTH
-		<< "] [threads="             << clovis::DEFAULT_BENCH_THREADS
-		<< "] [hash="                << clovis::DEFAULT_BENCH_MB << "]" << std::endl
+		<< "] [threads=" << clovis::DEFAULT_BENCH_THREADS
+		<< "] [hash=" << clovis::DEFAULT_BENCH_MB << "]" << std::endl
 		<< "search positions as specified by OpenBench" << std::endl;
 	else return EXIT_FAILURE;
 
