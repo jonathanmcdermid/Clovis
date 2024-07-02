498c0a43203058644b429964c46d836fe5ab6b9e
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2024-07-01 22:55:01 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main(const int argc, char* argv[])
     clovis::bitboards::init_bitboards();
 
     std::vector<std::string> args;
+    args.reserve(argc);
 
     for (int i = 0; i < argc; ++i) { args.emplace_back(argv[i]); }
 
