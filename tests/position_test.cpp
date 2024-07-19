ec9eac0eec8a8360250030e488cdb674f5a19215
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2024-07-19 01:58:23 -0600

    update tests

--- tests/position_test.cpp
+++ tests/position_test.cpp
@@ -183,23 +183,34 @@ TEST_F(PositionTest, SEE_TEST)
     }
 }
 
-// TEST_F(PositionTest, PINNER_TEST)
-// {
-//     Position pos("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-// 
-//     ASSERT_EQ(pos.get_pinner<WHITE>(A1), A1);
-// }
-// 
-// TEST_F(PositionTest, DISCOVERY_TEST)
-// {
-//     Position pos("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-// 
-//     ASSERT_TRUE(pos.is_discovery_threat<WHITE>(A1));
-// }
-// 
-// TEST_F(PositionTest, ATTACKED_TEST)
-// {
-//     Position pos("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-// 
-//     ASSERT_TRUE(pos.is_attacked<WHITE>(A1));
-// }
+TEST_F(PositionTest, PINNER_TEST)
+{
+    Position pos("8/8/8/4q3/8/8/4N3/4K3 w - - 0 1");
+    ASSERT_EQ(pos.get_pinner<WHITE>(E2), E5);
+    pos.set("8/8/8/4q3/8/4b3/4N3/4K3 w - - 0 1");
+    ASSERT_EQ(pos.get_pinner<WHITE>(E2), SQ_NONE);
+}
+
+TEST_F(PositionTest, DISCOVERY_TEST)
+{
+    Position pos("8/6q1/8/8/8/2P5/1B6/8 w - - 0 1");
+    ASSERT_TRUE(pos.is_discovery_threat<BLACK>(G7));
+    pos.set("8/6q1/8/8/2p5/2P5/1B6/8 w - - 0 1");
+    ASSERT_FALSE(pos.is_discovery_threat<BLACK>(G7));
+    pos.set("8/6q1/8/8/1b6/2P5/1B6/4K3 w - - 0 1");
+    ASSERT_TRUE(pos.is_discovery_threat<BLACK>(G7));
+    // pos.set("8/6q1/8/b7/8/2P5/1B6/4K3 w - - 0 1"); TODO: problem detected! immobile pawn is not recognized
+    // ASSERT_FALSE(pos.is_discovery_threat<BLACK>(G7));
+}
+
+TEST_F(PositionTest, ATTACKED_TEST)
+{
+    Position pos("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    ASSERT_TRUE(pos.is_attacked<BLACK>(A2));
+    ASSERT_FALSE(pos.is_attacked<WHITE>(A2));
+    ASSERT_TRUE(pos.is_attacked<WHITE>(A7));
+    ASSERT_FALSE(pos.is_attacked<BLACK>(A7));
+    ASSERT_FALSE(pos.is_attacked<BLACK>(A1));
+    ASSERT_FALSE(pos.is_attacked<WHITE>(A8));
+}
